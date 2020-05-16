329d5934642be7ea5e2a62b8241823c494841d81
eliran.david <eliran.david@nielsen.com>
2020-05-16 13:24:02 +0300

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+?
+routes/index.js,3/0/30dfc417d69af8ed298ed4111dcab68f38ca7f54
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+const rp = require('request-promise-native')
+var router = express.Router();
+
+router.get('/basic-node', async function (req, res, next) {
+  const options = {
+    method: 'GET',
+    url: 'http://localhost:8090/sleep/3000',
+    json: true
+  }
+  const response = await rp(options)
+  res.send('done');
+});
+
+module.exports = router;
